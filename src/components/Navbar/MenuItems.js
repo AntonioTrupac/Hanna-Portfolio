88d0466ece187cf173054b66d8e5ec27ad1bb516
AntonioTrupac <sniper.trupac@gmail.com>
2021-01-18 19:28:08 +0100

    Navbar commit

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,29 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'About me',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+
+        title: 'Portfolio',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+
+        title: 'Hire me',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Sign up',
+        url: '#',
+        cName: 'nav-links-mobile'
+    }
+]
